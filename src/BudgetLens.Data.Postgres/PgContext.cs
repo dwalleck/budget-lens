981061dc37dc3020c6a517cbd13be74cdaaa0b97
Daryl Walleck <dwalleck@gmail.com>
2025-02-17 12:35:29 -0600

    Start of adding dbcontext

--- src/BudgetLens.Data.Postgres/PgContext.cs
+++ src/BudgetLens.Data.Postgres/PgContext.cs
@@ -1,6 +1,10 @@
+using System.Transactions;
+
+using Microsoft.EntityFrameworkCore;
+
 namespace BudgetLens.Data.Postgres;
 
-public class PgContext
+public class PgContext(DbContextOptions<PgContext> options) : DbContext(options)
 {
-
+    public DbSet<Transaction> Transactions => Set<Transaction>();
 }
